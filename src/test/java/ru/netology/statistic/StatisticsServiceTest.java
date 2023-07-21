66dd933a2abc3d564d9c80a89678d23880dc8146
lina mayakovskaya <linamayakovskaya@mail.ru>
2023-07-22 01:07:30 +0300

    Initial commit

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -19,7 +19,7 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    void findLastMax() {
+    void findOneMax() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {4, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
